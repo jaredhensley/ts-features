1e07e5897db6a442220aba497fb1645033fd0348
Jared Hensley <jahensle85@me.com>
2019-07-15 23:17:23 -0500

    typed array inferances

---
+++ arrays.ts
@@ -0,0 +1,12 @@
+// infers cars: string[]
+const cars = ['ford', 'saab', 'bmw'];
+
+// infers dates: Date[]
+const dates = [new Date(), new Date(), new Date()];
+
+// infers twoDim: string[][]
+const twoDim = [
+  ['something'],
+  ['something else'],
+  ['something something else']
+];
