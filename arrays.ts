2a242af2b37f39a1fa4351516cc7d1d42287f45c
Jared Hensley <jahensle85@me.com>
2019-07-16 23:24:48 -0500

    more examples

--- arrays.ts
+++ arrays.ts
@@ -1,6 +1,12 @@
 // infers cars: string[]
 const cars = ['ford', 'saab', 'bmw'];
 
+// infers car: string
+const inferredCar = cars[0];
+
+// type inference error
+cars.push(100);
+
 // infers dates: Date[]
 const dates = [new Date(), new Date(), new Date()];
 
@@ -10,3 +16,6 @@ const twoDim = [
   ['something else'],
   ['something something else']
 ];
+
+// flexible array types
+const importantDate = [new Date(), '2020-11-11'];
